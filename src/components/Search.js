cf6c3571900a33bc71a2cbf496c3d3107feb2243
Mikel1991 <mikel362d@gmail.com>
2020-12-23 14:13:00 -0800

    creates search component, wikipedia API and axios Get method

---
+++ src/components/Search.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+const Search = () => {
+    const [term, setTerm] = useState("");
+
+    useEffect(() => {
+        const searchWiki = async () => {
+            await axios.get("https://en.wikipedia.org/w/api.php", {
+                params: {
+                    action: "query",
+                    list: "search",
+                    origin: "*",
+                    format: "json",
+                    srsearch: term,
+                },
+            })
+        }
+        searchWiki();
+    }, [term])
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Seacrh Term</label>
+                    <input 
+                        value={term}
+                        onChange={ e => setTerm(e.target.value)}
+                        className="input" 
+                    />
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Search;

d7bcdb05c811397c8b99234e789cffffdcef52e2
Mikel1991 <mikel362d@gmail.com>
2020-12-23 13:01:58 -0800

    adds basic clickable functionality to dropdown in accordion file

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,16 +1,25 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map(item => {
-        return <div key={item.title}>
-            <div className="title active">
-                <i className="dropdown icon"></i>
-                {item.title}
-            </div>
-            <div className="content active">
-                <p>{item.content}</p>
-            </div>
-        </div>
+    const onTitleclick = (index) => {
+        console.log("title clicked", index);
+    };
+
+    const renderedItems = items.map((item, index) => {
+        return (
+            <React.Fragment key={item.title}>
+                <div 
+                    className="title active"
+                    onClick={()=> onTitleclick(index)}
+                >
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    <p>{item.content}</p>
+                </div>
+            </React.Fragment>
+        )
     });
     return <div className="ui styled accordion">
         {renderedItems}
